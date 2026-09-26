8c259d3516f87608a315b6d214647e954be3e922
firojhasan328-pixel <firojhasan328@gmail.com>
2026-09-26 10:21:11 +0600

    Create authService.js

---
+++ src/services/authService.js
@@ -0,0 +1,29 @@
+import { supabase } from "../lib/supabaseClient";
+
+// Login
+export async function signInWithEmail(email, password) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+  if (error) throw error;
+  return data;
+}
+
+// Signup
+export async function signUpWithEmail(name, email, password) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: { full_name: name },
+    },
+  });
+  if (error) throw error;
+  return data;
+}
+
+// Logout
+export async function signOut() {
+  await supabase.auth.signOut();
+}

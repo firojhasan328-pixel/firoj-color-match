cfbe508a1e2d64b9f0e8a566ed92bb9d5284e180
firojhasan328-pixel <firojhasan328@gmail.com>
2026-09-26 10:23:37 +0600

    Create Logo.jsx

---
+++ src/components/Logo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Logo() {
+  return (
+    <div className="logo-wrap">
+      <div className="logo-mark">
+        <span className="dot dot-1" />
+        <span className="dot dot-2" />
+        <span className="dot dot-3" />
+      </div>
+      <h1 className="logo-text">Color Match</h1>
+    </div>
+  );
+}
